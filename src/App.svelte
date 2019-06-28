aa5c7c09f1a068f3d399d618cca53d4cc1304d3f
Vladimir <n_vova@mail.ru>
2019-06-29 06:51:10 +0800

    6. Build the brick field

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,37 @@
 		var paddleHeight = 10;
 		var paddleWidth = 75;
 		var paddleX = (canvas.width-paddleWidth)/2;
+		var brickRowCount = 3;
+		var brickColumnCount = 5;
+		var brickWidth = 75;
+		var brickHeight = 20;
+		var brickPadding = 10;
+		var brickOffsetTop = 30;
+		var brickOffsetLeft = 30;
+		var bricks = [];
+
+		for(var c=0; c<brickColumnCount; c++) {
+			bricks[c] = [];
+			for(var r=0; r<brickRowCount; r++) {
+				bricks[c][r] = { x: 0, y: 0 };
+			}
+		}
+
+		function drawBricks() {
+			for(var c=0; c<brickColumnCount; c++) {
+				for(var r=0; r<brickRowCount; r++) {
+					var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+					bricks[c][r].x = brickX;
+					bricks[c][r].y = brickY;
+					ctx.beginPath();
+					ctx.rect(brickX, brickY, brickWidth, brickHeight);
+					ctx.fillStyle = "#0095DD";
+					ctx.fill();
+					ctx.closePath();
+				}
+			}
+		}
 
 		function drawBall() {
 			ctx.beginPath();
@@ -36,6 +67,7 @@
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			drawBall();
 			drawPaddle();
+			drawBricks();
 
 			if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 				dx = -dx;
