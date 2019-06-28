e56b8bab29aa32b522c31323fd5e52cdf41b9aaf
Vladimir <n_vova@mail.ru>
2019-06-29 06:53:05 +0800

    7. Collision detection

--- src/App.svelte
+++ src/App.svelte
@@ -27,22 +27,38 @@
 		for(var c=0; c<brickColumnCount; c++) {
 			bricks[c] = [];
 			for(var r=0; r<brickRowCount; r++) {
-				bricks[c][r] = { x: 0, y: 0 };
+				bricks[c][r] = { x: 0, y: 0, status: 1 };
+			}
+		}
+
+		function collisionDetection() {
+			for(var c=0; c<brickColumnCount; c++) {
+				for(var r=0; r<brickRowCount; r++) {
+					var b = bricks[c][r];
+					if(b.status == 1) {
+						if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+							dy = -dy;
+							b.status = 0;
+						}
+					}
+				}
 			}
 		}
 
 		function drawBricks() {
 			for(var c=0; c<brickColumnCount; c++) {
 				for(var r=0; r<brickRowCount; r++) {
-					var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-					bricks[c][r].x = brickX;
-					bricks[c][r].y = brickY;
-					ctx.beginPath();
-					ctx.rect(brickX, brickY, brickWidth, brickHeight);
-					ctx.fillStyle = "#0095DD";
-					ctx.fill();
-					ctx.closePath();
+					if(bricks[c][r].status == 1) {
+						var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+						var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+						bricks[c][r].x = brickX;
+						bricks[c][r].y = brickY;
+						ctx.beginPath();
+						ctx.rect(brickX, brickY, brickWidth, brickHeight);
+						ctx.fillStyle = "#0095DD";
+						ctx.fill();
+						ctx.closePath();
+					}
 				}
 			}
 		}
@@ -65,9 +81,10 @@
 
 		function draw() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			drawBricks();
 			drawBall();
 			drawPaddle();
-			drawBricks();
+			collisionDetection();
 
 			if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 				dx = -dx;
