8ba206661307d66fca3709ee4b0c7a94ead5dc06
Vladimir <n_vova@mail.ru>
2019-06-28 20:25:57 +0800

    3. Bounce off the walls

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,10 @@
 	import { onMount } from 'svelte';
 
 	let canvas;
-	
+
 	onMount(() => {
-		const ctx = canvas.getContext('2d');		
+		const ctx = canvas.getContext('2d');
+		var ballRadius = 10;
 		var x = canvas.width/2;
 		var y = canvas.height-30;
 		var dx = 2;
@@ -12,7 +13,7 @@
 
 		function drawBall() {
 			ctx.beginPath();
-			ctx.arc(x, y, 10, 0, Math.PI*2);
+			ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 			ctx.fillStyle = "#0095DD";
 			ctx.fill();
 			ctx.closePath();
@@ -21,6 +22,14 @@
 		function draw() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			drawBall();
+
+			if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+				dx = -dx;
+			}
+			if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+				dy = -dy;
+			}
+
 			x += dx;
 			y += dy;
 		}
