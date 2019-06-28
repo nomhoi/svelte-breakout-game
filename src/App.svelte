a891e9325c8554cda9f8fbd36beaed019ade7b30
Vladimir <n_vova@mail.ru>
2019-06-28 20:34:31 +0800

    4. Paddle and keyboard controls

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 	import { onMount } from 'svelte';
 
 	let canvas;
+	var rightPressed = false;
+	var leftPressed = false;
 
 	onMount(() => {
 		const ctx = canvas.getContext('2d');
@@ -10,6 +12,9 @@
 		var y = canvas.height-30;
 		var dx = 2;
 		var dy = -2;
+		var paddleHeight = 10;
+		var paddleWidth = 75;
+		var paddleX = (canvas.width-paddleWidth)/2;
 
 		function drawBall() {
 			ctx.beginPath();
@@ -19,9 +24,18 @@
 			ctx.closePath();
 		}
 
+		function drawPaddle() {
+			ctx.beginPath();
+			ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
+
 		function draw() {
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			drawBall();
+			drawPaddle();
 
 			if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 				dx = -dx;
@@ -30,13 +44,40 @@
 				dy = -dy;
 			}
 
+			if(rightPressed && paddleX < canvas.width-paddleWidth) {
+				paddleX += 7;
+			}
+			else if(leftPressed && paddleX > 0) {
+				paddleX -= 7;
+			}
+
 			x += dx;
 			y += dy;
 		}
 
 		setInterval(draw, 10);
 	});
+
+	function keyDownHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = true;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = true;
+		}
+	}
+
+	function keyUpHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = false;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = false;
+		}
+	}	
 </script>
+
+<svelte:window on:keydown={keyDownHandler} on:keyup={keyUpHandler}/>
 
 <canvas
 	bind:this={canvas}
