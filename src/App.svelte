e263bbeeedb408f3035a70709b364b98c6b2c228
Vladimir <n_vova@mail.ru>
2019-06-28 20:22:38 +0800

    2. Move the ball

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,30 @@
 	import { onMount } from 'svelte';
 
 	let canvas;
-
+	
 	onMount(() => {
-		const ctx = canvas.getContext('2d');
-		
-		ctx.beginPath();
-		ctx.rect(20, 40, 50, 50);
-		ctx.fillStyle = "#FF0000";
-		ctx.fill();
-		ctx.closePath();
+		const ctx = canvas.getContext('2d');		
+		var x = canvas.width/2;
+		var y = canvas.height-30;
+		var dx = 2;
+		var dy = -2;
 
-		ctx.beginPath();
-		ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-		ctx.fillStyle = "green";
-		ctx.fill();
-		ctx.closePath();
+		function drawBall() {
+			ctx.beginPath();
+			ctx.arc(x, y, 10, 0, Math.PI*2);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
 
-		ctx.beginPath();
-		ctx.rect(160, 10, 100, 40);
-		ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-		ctx.stroke();
-		ctx.closePath();
+		function draw() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			drawBall();
+			x += dx;
+			y += dy;
+		}
+
+		setInterval(draw, 10);
 	});
 </script>
 
